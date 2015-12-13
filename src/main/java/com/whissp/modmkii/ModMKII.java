24d45ab2e81ddd59ff64274cc776d4d68956b19e
whissp <gage.gosset@gmail.com>
2015-12-13 17:49:16 -0600

    commit 1

---
+++ src/main/java/com/whissp/modmkii/ModMKII.java
@@ -0,0 +1,33 @@
+package com.whissp.modmkii;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="modmki",name="modmki",version="1.7.2-1.0")
+public class ModMKII
+{
+    @Mod.Instance("modmki")
+    public static ModMKII instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+
+}
+
